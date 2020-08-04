3949b88507e3c67f25d4546dcaa7755ce2b6736e
illias <khomko.illia@gmail.com>
2020-08-04 10:18:54 +0900

    link to menu from home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 type IHomeProps = {};
 
 const Home: React.FC<IHomeProps> = ({ ...props }) => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <br />
+      <Link to="/sample/2">to menu</Link>
+    </div>
+  );
 };
 
 export default Home;
