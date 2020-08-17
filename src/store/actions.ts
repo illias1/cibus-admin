7e33eeeb3baf812080141fe6b194073b99dec0eb
illias <khomko.illia@gmail.com>
2020-08-17 11:49:26 +0200

    fix store

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,14 +1,3 @@
 import actionCreatorFactory from "typescript-fsa";
-import { TCartItem, TCartItemStatus } from "./types";
 
 const actionCreator = actionCreatorFactory();
-
-export const setUserName = actionCreator<string>("setUserName");
-export const setUserAlreadyVisited = actionCreator<boolean>(
-  "setUserAlreadyVisited"
-);
-export const addToCart = actionCreator<TCartItem>("addToCart");
-export const removeItemFromCart = actionCreator<string>("removeItemFromCart");
-export const setCartItemsStatus = actionCreator<TCartItemStatus>(
-  "setCartItemsStatus"
-);
--- src/store/state.ts
+++ src/store/state.ts
@@ -1,36 +1,3 @@
-import { TStore, TCartItem, TGroupCartItem } from "./types";
-import {
-  LOCAL_STORAGE_USER_NAME,
-  LOCAL_STORAGE_USER_ALREADY_VISITED,
-  LOCAL_STORAGE_CART,
-} from "../utils/_constants";
+import { TStore } from "./types";
 
-const sampleGroupCart: TGroupCartItem[] = [
-  {
-    customerName: "Penny",
-    price: 44.95,
-    tip: 4.5,
-  },
-  {
-    customerName: "Raj",
-    price: 17.98,
-    tip: 4.5,
-  },
-  {
-    customerName: "Leonard",
-    price: 0,
-    tip: 0,
-  },
-];
-
-export const initialState: TStore = {
-  userName: localStorage.getItem(LOCAL_STORAGE_USER_NAME) || "",
-  userAlreadyVisited: localStorage.getItem(LOCAL_STORAGE_USER_ALREADY_VISITED)
-    ? true
-    : false,
-  cart:
-    (JSON.parse(
-      localStorage.getItem(LOCAL_STORAGE_CART) || "[]"
-    ) as TCartItem[]) || [],
-  groupCart: sampleGroupCart,
-};
+export const initialState: TStore = {};
