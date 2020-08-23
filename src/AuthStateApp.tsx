02135bb2aab1c187ab75152c8514687b7879f03a
illias <khomko.illia@gmail.com>
2020-08-24 01:05:15 +0200

    aws-configure and auth flow done

---
+++ src/AuthStateApp.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+// Authentication
+import { AmplifyAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import { Typography, makeStyles, Theme, createStyles } from "@material-ui/core";
+import App from "./App";
+import background from "./assets/background.png";
+
+const AuthStateApp: React.FunctionComponent = () => {
+  const [authState, setAuthState] = React.useState<AuthState>();
+  const [user, setUser] = React.useState<object | undefined>();
+  const classes = useStyles();
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
+    <App />
+  ) : (
+    <div className={classes.auth}>
+      <div className={classes.title}>
+        <Typography variant="h1">Welcome to</Typography>
+        <Typography variant="h1">cibus.online</Typography>
+      </div>
+      <AmplifyAuthenticator usernameAlias="email">
+        <AmplifySignUp
+          usernameAlias="email"
+          slot="sign-up"
+          formFields={[{ type: "email" }, { type: "password" }]}
+        />
+      </AmplifyAuthenticator>
+    </div>
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    auth: {
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "flex-end",
+      height: "100%",
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), #000),
+          url(${background})`,
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      paddingRight: theme.spacing(5),
+    },
+    title: {
+      position: "absolute",
+      top: "20%",
+      color: "white",
+      left: "10%",
+    },
+  })
+);
+
+export default AuthStateApp;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./i18n";
-import App from "./App";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "./utils/theme";
+import { CssBaseline } from "@material-ui/core";
+import AuthStateApp from "./AuthStateApp";
+import { BrowserRouter } from "react-router-dom";
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <CssBaseline />
+          <AuthStateApp />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
